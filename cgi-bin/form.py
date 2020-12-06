4426390571b456f6305a6d4a61979567c8d08067
Konstantin-Lis <55928181+Konstantin-Lis@users.noreply.github.com>
2020-12-06 19:47:51 +0300

    Create form.py

---
+++ cgi-bin/form.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+print("Content-type: text/html\n")
+print("""<!DOCTYPE HTML>
+        <html>
+        <head>
+            <meta charset="utf-8">
+            <title>Создать капчу</title>
+        </head>
+        <body>""")
+
+print("<h1>Кнопка сработала</h1>")
+
+print("<form action='img.py'>")
+
+print("<p><image src = 'umbrella.gif' width = 20, height = 20, alt = 'Что-то не так'>")
+print("<image src = 'umbrella.gif' width = 20, height = 20, alt = 'Что-то не так'>")
+print("<image src = 'umbrella.gif' width = 20, height = 20, alt = 'Что-то не так'></p>")
+
+print("<p><image src = 'umbrella.gif' width = 20, height = 20, alt = 'Что-то не так'>")
+print("<image src = 'umbrella.gif' width = 20, height = 20, alt = 'Что-то не так'>")
+print("<image src = 'umbrella.gif' width = 20, height = 20, alt = 'Что-то не так'></p>")
+
+print("<p><image src = 'umbrella.gif' width = 20, height = 20, alt = 'Что-то не так'>")
+print("<image src = 'umbrella.gif' width = 20, height = 20, alt = 'Что-то не так'>")
+print("<image src = 'umbrella.gif' width = 20, height = 20, alt = 'Что-то не так'></p>")
+
+print("</form>")
+
+print("""</body>
+        </html>""")
