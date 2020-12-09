d4a4bce8fbed811057a6932c84072b5829bbf0f6
Konstantin-Lis <55928181+Konstantin-Lis@users.noreply.github.com>
2020-12-09 15:33:15 +0300

    Update Parser.py

--- Parser.py
+++ Parser.py
@@ -1,6 +1,23 @@
 from selenium import webdriver
 import cv2
 
+from tensorflow.keras.models import load_model
+model = load_model("Cars_and_Roads9857.h5")
+
+def predict_image(img_path):
+    # Read the image and resize it
+    img = image.load_img(img_path, target_size=(90, 90))
+    # Convert it to a Numpy array with target shape.
+    x = image.img_to_array(img)
+    # Reshape
+    x = x.reshape((1,) + x.shape)
+    x /= 255.
+    result = model.predict([x])[0][0]
+    if result > 0.5:
+        return 1
+    else:
+        return 0
+
 driver = webdriver.Chrome("C://Users/USER/PycharmProjects/pythonProject/chromedriver.exe")
 driver.get("localhost:8000")
 
@@ -15,4 +32,8 @@ for i in range(9):
     loc = images[i].location
     img = image[loc['y']+46+30*(i//3):loc['y']+46+30*(i//3)+90, loc['x']+2+30*(i%3):loc['x']+2+30*(i%3)+90]
     cv2.imwrite("img_"+str(i)+".jpg", img)
-driver.quit()
+    ans = predict_image("img_"+str(i)+".jpg")
+    if ans == 1:
+        images[i].click()
+
+#driver.quit()
